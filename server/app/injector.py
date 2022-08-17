9c27ba2350e66a470928171328a8f1c09ed1da34
Daniil <daniil.petro3@gmail.com>
2022-08-17 12:28:38 +0500

    fix injector

--- server/app/injector.py
+++ server/app/injector.py
@@ -1,12 +1,12 @@
 import re
 
-containers_map = {}
 
 def to_snake_case(name):
     name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     name = re.sub('__([A-Z])', r'_\1', name)
     name = re.sub('([a-z0-9])([A-Z])', r'\1_\2', name)
     return name.lower()
+
 
 class Container:
     def override(self, dependency, new_dependency):
@@ -21,21 +21,28 @@ class Container:
     def __setitem__(self, key, value):
         return setattr(self, key, value)
 
+
+containers_map: dict[str, Container] = {}
+
+
 def inject(name: str, *args):
     if not name or type(name).__name__ != 'str':
         raise ValueError('Provide name for inject')
+
     def inner(fn):
         container = Container()
         containers_map[name] = container
         for dep in args:
             container.add_dependency(dep)
+
         def inner1(self, **kwargs):
-            new_args = { **kwargs, 'container': container }
+            new_args = {**kwargs, 'container': container}
             fn(self, **new_args)
-        
+
         return inner1
 
     return inner
 
-def get_container(name: str) -> Container:
+
+def get_container(name: str) -> Container | None:
     return containers_map.get(name)
