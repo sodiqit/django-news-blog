90c2fc10c8b3d8b9557f24fc60a6abf5a42e4aef
Daniil <daniil.petro3@gmail.com>
2022-09-06 12:30:08 +0500

    implement filtering by 'all' lookup

--- server/app/apps/core/filter.py
+++ server/app/apps/core/filter.py
@@ -18,7 +18,8 @@ class QueryFilter:
         params = [self._parse_param(name, value) if self._is_valid(name) else None for name, value in request.query_params.items()]
         params = filter(lambda x: x is not None, params) #type: ignore
         params = reduce(lambda x,y: {**x, **y}, params, {}) #type: ignore
-        return queryset.filter(**params)
+        extracted_keys = self._omit_fields_by_all_filtering(params)
+        return self._filter_by_all_key(queryset.filter(**params), extracted_keys)
 
     '''Define which query fields must be support in filtering'''
     @property
@@ -28,22 +29,6 @@ class QueryFilter:
     @fields.setter
     def fields(self, fields: list[str]):
         self._fields = fields
-
-    # def _is_valid(self, name: str) -> bool:
-    #     field_name, *args = name.split('__')
-    
-    #     try:
-    #         field = list(filter(lambda x: x['name'] == field_name, self._fields))[0]
-
-    #         def validate_lookup(lookup: str):
-    #             return lookup in args
-
-    #         if field['lookups'] == '__all__' or field['lookups'] is None:
-    #             return False
-
-    #         return any([True if validate_lookup(lookup) else False for lookup in field['lookups']])
-    #     except:
-    #         return False
 
     def _is_valid(self, name: str) -> bool:
         field_name = name.split('__')[0]
@@ -61,3 +46,23 @@ class QueryFilter:
             pass
 
         return { name: value } 
+
+    def _omit_fields_by_all_filtering(self, params):
+        result = {}
+        cloned_params = params.copy()
+        for name, value in cloned_params.items():
+            field_name, *args = name.split('__')
+            if 'all' in args:
+                result[field_name] = value
+                params.pop(name)
+
+        return result
+
+    def _filter_by_all_key(self, queryset, params: dict):
+        res = queryset
+        for field_name, value in params.items():
+            for i in value:
+                res = res.filter(**{f'{field_name}__id': i})
+
+        return res
+            
