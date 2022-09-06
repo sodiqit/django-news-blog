b1659ce962666632b8c06733f3f79bbbdbf92226
Daniil <daniil.petro3@gmail.com>
2022-09-06 12:31:12 +0500

    connect view for sort and filtering

--- server/app/apps/posts/views.py
+++ server/app/apps/posts/views.py
@@ -3,11 +3,14 @@ from rest_framework import viewsets, serializers
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, SAFE_METHODS, BasePermission
 
-from app.apps.core.utils import convert_categories_to_tree
+from app.apps.core.converter import CategoryConverter
 from app.apps.core.models import Category
 from app.injector import inject
 from app.fp import pipe
 from app.apps.core.filter import QueryFilter
+from django.db.models import QuerySet
+
+from app.apps.core.sort import Sort
 
 from .serializers import PostSerializer
 from .models import MODERATE_STATUSES, Post
@@ -26,6 +29,9 @@ class IsOwnerOrReadOnly(BasePermission):
 @dataclass
 class PostViewContainer:
     query_filter: QueryFilter
+    sort: Sort
+    category_converter: CategoryConverter
+
 
 class PostView(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
@@ -33,31 +39,35 @@ class PostView(viewsets.ModelViewSet):
         draft__moderate_status=MODERATE_STATUSES.APPROVED)
     serializer_class = PostSerializer
 
-    @inject('PostView', QueryFilter)
+    @inject('PostView', QueryFilter, Sort, CategoryConverter)
     def __init__(self, **kwargs) -> None:
         self.container: PostViewContainer = kwargs.pop('container')
         self.query_filter = self.container.query_filter
         self.query_filter.fields = [
             'id', 'title', 'author', 'categories', 'tags', 'created_date']
+        self.sort = self.container.sort
+        self.sort.fields = ['id', 'categories', 'images']
         super().__init__(**kwargs)
 
     def list(self, request):
-        queryset = pipe(self.get_queryset(), self.filter_queryset,
-                        self.query_filter.filter_by_fields(request=request))
+        queryset: QuerySet = pipe(self.get_queryset(), self.filter_queryset,
+                                  self.query_filter.filter_by_fields(request=request), self.sort.sort_by_fields(request=request))
 
         page = self.paginate_queryset(queryset)
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(self._add_categories_tree(serializer))
 
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(self._add_categories_tree(serializer))
+        result = pipe(self.get_serializer(queryset, many=True),
+                      self._add_categories_tree)
+        return Response(result)
 
     def _add_categories_tree(self, serializer: serializers.BaseSerializer):
         for post in serializer.data:
             categories = [Category.objects.get(
                 id=c['id']) for c in post['categories']]
-            category_tree = convert_categories_to_tree(categories)
+            category_tree = self.container.category_converter.convert_categories_to_tree(
+                categories)
             post['categories'] = category_tree
 
         return serializer.data
